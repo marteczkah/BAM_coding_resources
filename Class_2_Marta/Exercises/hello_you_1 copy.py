3226c37c002566cd2aa661df4a6210c3b8bc5fad
Kobie Thomas <kobiethomas@kobies-mbp.home>
2021-07-26 22:40:12 -0400

    add python exercises

---
+++ Class_2_Marta/Exercises/hello_you_1 copy.py
@@ -0,0 +1,16 @@
+#Ask user for name and assign it to the variable name
+name = input("What is your name?:  ")
+
+
+#Ask user for age and assign it to the variable age
+age = input("What is you age?: ")
+
+
+#Ask user for city and assign it to the variable city
+city = input("What city are you from?: ")
+
+#Ask user what they enjoy and assign it to the variable hobbies
+hobbies = input("What are your hobbies?: ")
+
+#"Your name is {} and you are {} years old. You live in {} and you love {}"
+#assign to a string c
